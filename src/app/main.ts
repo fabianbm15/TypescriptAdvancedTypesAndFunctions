4201b92202b1d69e0619efc115c108e07bc86e3f
fabianbm15 <cmelgarejobm@gmail.com>
2023-05-09 08:03:02 -0500

    extends interface

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,14 +1,18 @@
 import { addProduct } from './products/product.service';
+import { products } from './products/product.service';
 
 addProduct({
   id: '2',
   title: 'p2',
   createdAt: new Date(),
+  updatedAt: new Date(),
   stock: 50,
   size: 'XL',
   category: {
     id: '12',
     name: 'c1',
+    createdAt: new Date(),
+    updatedAt: new Date(),
   },
 });
 
