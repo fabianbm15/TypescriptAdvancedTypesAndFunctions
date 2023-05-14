64b64f8cda6ecaaf3a3652b68587bfcb44a72d65
fabianbm15 <cmelgarejobm@gmail.com>
2023-05-14 07:50:35 -0500

    readonly - propiedades de solo lectura

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -3,5 +3,6 @@ import { Product } from './product.model';
 export const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
+  // data.id = '1' // No me permite porque es solo readonly
   products.push(data);
 };
